0dc084e20e06d53eb1ec587a584ef010899950ae
ngyngcphu <nguyenngocphu225@gmail.com>
2023-03-27 17:13:13 +0700

    warning failure on form and show/hidden password

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,7 +1,6 @@
-import { ChangeEvent, FormEvent } from "react";
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState, useEffect } from "react";
 import { Button, Label, Modal, TextInput } from "flowbite-react";
-import { ExclamationCircleIcon /*EyeIcon, EyeSlashIcon*/ } from "@heroicons/react/24/solid";
+import { ExclamationCircleIcon, EyeIcon, EyeSlashIcon, UserIcon, LockClosedIcon } from "@heroicons/react/24/solid";
 import img from "../assets/login.png";
 
 interface InitialFormValues {
@@ -14,8 +13,18 @@ interface FormError {
   password: string;
 }
 
+interface ValidColor {
+  username: string;
+  password: string;
+}
+
 export function LoginPage() {
-  const [show, setShow] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [showPassword, setShowPassword] = useState("password");
+  const [validColor, setValidColor] = useState<ValidColor>({
+    username: "grey",
+    password: "grey"
+  });
   const [formValues, setFormValues] = useState<InitialFormValues>({
     username: "",
     password: ""
@@ -24,14 +33,13 @@ export function LoginPage() {
     username: "",
     password: ""
   });
-  // const [showPassword, setShowPassword] = useState(false);
 
   const onClick = () => {
-    setShow(true);
+    setShowModal(true);
   };
 
   const onClose = () => {
-    setShow(false);
+    setShowModal(false);
   };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -39,9 +47,13 @@ export function LoginPage() {
     setFormValues({ ...formValues, [id]: value });
   };
 
-  // const handleShowPassword = () => {
-  //   setShowPassword(!showPassword);
-  // }
+  const handleShowPassword = () => {
+    if (showPassword === "password") {
+      setShowPassword("text");
+    } else {
+      setShowPassword("password");
+    }
+  };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -64,6 +76,52 @@ export function LoginPage() {
     return errors;
   };
 
+  useEffect(() => {
+    if (formError.username.length > 0) {
+      setValidColor((prevState) => ({
+        ...prevState,
+        username: "failure"
+      }));
+    } else {
+      setValidColor((prevState) => ({
+        ...prevState,
+        username: "grey"
+      }));
+    }
+
+    if (formError.password.length > 0) {
+      setValidColor((prevState) => ({
+        ...prevState,
+        password: "failure"
+      }));
+    } else {
+      setValidColor((prevState) => ({
+        ...prevState,
+        password: "grey"
+      }));
+    }
+  }, [formError]);
+
+  const handleFocusUsername = () => {
+    if (validColor.username === "failure") {
+      setValidColor((prevState) => ({
+        ...prevState,
+        username: "grey"
+      }));
+    }
+  };
+
+  const handleFocusPassword = () => {
+    if (validColor.password === "failure") {
+      setValidColor((prevState) => ({
+        ...prevState,
+        password: "grey"
+      }));
+    }
+  };
+
+  console.log("Hello");
+
   return (
     <div className='bg-[#F8F8F8] h-screen'>
       <div className='container mx-auto h-screen bg-[#DDF3F2]'>
@@ -80,27 +138,36 @@ export function LoginPage() {
               <TextInput
                 className='mx-28'
                 sizing='lg'
-                color='grey'
+                color={validColor.username}
                 id='username'
                 value={formValues.username}
                 type='text'
                 placeholder='Username'
+                icon={UserIcon}
                 onChange={handleChange}
+                onFocus={handleFocusUsername}
               />
               <p className='ml-32 mb-5 font-archivo text-[#F12323] text-[16px]'>{formError.username}</p>
               <Label className='ml-32 block text-[20px] font-archivo' htmlFor='password' style={{ color: "#444444" }}>
                 Password <span className='text-[#F12323]'>*</span>
               </Label>
-              <TextInput
-                className='mx-28'
-                sizing='lg'
-                color='grey'
-                id='password'
-                value={formValues.password}
-                type='password'
-                placeholder='Password'
-                onChange={handleChange}
-              />
+              <div className='relative'>
+                <TextInput
+                  className='mx-28'
+                  sizing='lg'
+                  color={validColor.password}
+                  id='password'
+                  value={formValues.password}
+                  type={showPassword}
+                  placeholder='Password'
+                  icon={LockClosedIcon}
+                  onChange={handleChange}
+                  onFocus={handleFocusPassword}
+                />
+                <span className='absolute inset-y-0 flex items-center right-32' onClick={handleShowPassword}>
+                  {showPassword === "text" ? <EyeIcon className='h-6 ml' /> : <EyeSlashIcon className='h-6' />}
+                </span>
+              </div>
               <p className='ml-32 font-archivo text-[#F12323] text-[16px]'>{formError.password}</p>
               <Button className='mx-32 mt-5 font-archivo' type='submit' size='md' style={{ backgroundColor: "#05A59D" }}>
                 Log in
@@ -115,7 +182,7 @@ export function LoginPage() {
                 Create new account
               </Button>
             </div>
-            <Modal show={show} size='md' popup={true} onClose={onClose}>
+            <Modal show={showModal} size='md' popup={true} onClose={onClose}>
               <Modal.Body>
                 <div className='text-center'>
                   <ExclamationCircleIcon className='mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200' />
