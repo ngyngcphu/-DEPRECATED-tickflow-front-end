73d318ac0d3e9d9d148d8ea1b1458a7da63dc8bf
ngyngcphu <nguyenngocphu225@gmail.com>
2023-04-01 00:50:37 +0700

    done navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "./components/LoginPage";
-import { SideBar } from "./components/SideBar";
+//import { SideBar } from "./components/SideBar";
+import { NavBar } from "./components/NavBar";
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<LoginPage />} />
-      <Route path='/overview' element={<SideBar />} />
+      <Route path='/overview' element={<NavBar />} />
     </Routes>
   );
 }
