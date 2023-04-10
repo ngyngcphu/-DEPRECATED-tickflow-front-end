5d06f586b1a1cb96641ae468ec14ad4e02f05fd2
ngyngcphu <nguyenngocphu225@gmail.com>
2023-04-10 20:21:53 +0700

    fix NotiParams

--- src/components/pages/projects/formparams/NotiParams.tsx
+++ src/components/pages/projects/formparams/NotiParams.tsx
@@ -1,23 +1,28 @@
 import { ChangeEvent, useState } from "react";
-import { Button, Label, Select, TextInput } from "flowbite-react";
-import { DocumentArrowUpIcon } from "@heroicons/react/24/solid";
+import { Button, Checkbox, Label, Select, TextInput } from "flowbite-react";
 import { EmailProposalProps, EmailProposal } from "../templates/EmailNoti";
 import { TemplatesName } from "../templates/TemplatesName";
 
 export function ProposalParams() {
+  const member: Array<string> = ["Nguyễn Thanh Hiền", "Nguyễn Hồng Quân", "Hoàng Lương", "Nguyễn Ngọc Phú"];
+  const mentor: Array<string> = ["Nguyễn Phúc Vinh", "Cù Đỗ Thanh Nhân", "Vũ Nguyễn Minh Huy", "Ngô Minh Hồng Thái"];
+
   const [params, setParams] = useState<EmailProposalProps>({
     subject: "",
-    receiver: ""
+    receiver: "",
+    timeSchedule: "",
+    dateSchedule: "",
+    link: ""
   });
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { id, value } = event.target;
     setParams({ ...params, [id]: value });
   };
 
   return (
     <div className='grid grid-cols-5'>
-      <div className='grid col-span-2 items-start gap-5'>
+      <div className='grid col-span-2 items-start gap-2'>
         <div className='bg-[#CCCCCC]'>
           <div className='p-4'>
             <Label htmlFor='templateName'>
@@ -30,8 +35,8 @@ export function ProposalParams() {
             </Select>
           </div>
         </div>
-        <div className='grid overflow-y-scroll h-96 p-4 gap-5'>
-          <div>
+        <div className='grid grid-cols-2 overflow-y-scroll h-96 p-4 gap-5'>
+          <div className='col-span-2'>
             <Label htmlFor='title'>
               Tiêu đề nội dung<span className='text-[#F12323]'>*</span>
             </Label>
@@ -41,43 +46,92 @@ export function ProposalParams() {
             <Label htmlFor='sender'>
               Người gửi<span className='text-[#F12323]'>*</span>
             </Label>
-            <TextInput id='sender' name='sender' />
+            <Select id='sender' required={true} defaultValue='---Chọn---(bắt buộc)'>
+              <option disabled>---Chọn---(bắt buộc)</option>
+              {member.map((member, index) => (
+                <option key={index}>{member}</option>
+              ))}
+            </Select>
           </div>
           <div>
             <Label htmlFor='receiver'>
               Người nhận<span className='text-[#F12323]'>*</span>
             </Label>
-            <TextInput id='receiver' name='receiver' value={params.receiver} onChange={handleChange} />
+            {/* <TextInput id='receiver' name='receiver' value={params.receiver} onChange={handleChange} /> */}
+            <Select id='receiver' name='receiver' value={params.receiver} onChange={handleChange} required={true}>
+              <option>---Chọn---(bắt buộc)</option>
+              {mentor.map((mentor, index) => (
+                <option key={index}>{mentor}</option>
+              ))}
+            </Select>
+          </div>
+          <div>
+            <Label htmlFor='CC'>CC</Label>
+            <Select id='CC'>
+              <option>(Optional)</option>
+              {member.map((member, index) => (
+                <option key={index}>{member}</option>
+              ))}
+            </Select>
+          </div>
+          <div>
+            <Label htmlFor='BCC'>BCC</Label>
+            <Select id='BCC'>
+              <option>(Optional)</option>
+              {member.map((member, index) => (
+                <option key={index}>{member}</option>
+              ))}
+            </Select>
           </div>
           <div>
             <Label htmlFor='via'>
-              Gửi qua<span className='text-[#F12323]'>*</span>
+              Send via<span className='text-[#F12323]'>*</span>
             </Label>
-            <TextInput id='via' name='via' />
+            <Select id='via' required={true}>
+              <option>Email</option>
+              <option>Slack</option>
+            </Select>
           </div>
           <div>
             <Label htmlFor='schedule'>
               Lịch hẹn<span className='text-[#F12323]'>*</span>
             </Label>
-            <div className='flex flex-row space-x-2'>
-              <TextInput className='basis-1/4' id='timeSchedule' name='timeSchedule' type='time' />
-              <TextInput className='basis-3/4' id='dateSchedule' name='dateSchedule' type='date' />
+            <div className='flex flex-row'>
+              <TextInput
+                className='basis-1/4'
+                id='timeSchedule'
+                name='timeSchedule'
+                type='time'
+                value={params.timeSchedule}
+                onChange={handleChange}
+              />
+              <TextInput
+                className='basis-3/4'
+                id='dateSchedule'
+                name='dateSchedule'
+                type='date'
+                value={params.dateSchedule}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div>
-            <Label htmlFor='link'>Đường link</Label>
-            <TextInput id='link' name='link' />
+            <Label htmlFor='link'>Link tài liệu</Label>
+            <TextInput id='link' name='link' value={params.link} onChange={handleChange} />
           </div>
           <div>
-            <Label htmlFor='attach'>Tài liệu đính kèm</Label>
-            <div className='flex w-full items-center justify-center'>
-              <label className='flex h-32 w-full cursor-pointer flex-col rounded border-2 border-dashed border-gray-300 hover:bg-gray-200 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-400'>
-                <div className='flex flex-col items-center justify-center pt-5 pb-6'>
-                  <DocumentArrowUpIcon className='w-12 text-gray-300' />
-                  <p className='py-1 text-sm text-gray-600 dark:text-gray-500'>Thả tệp tài liệu tại đây</p>
-                </div>
-                <input type='file' className='hidden' />
-              </label>
+            <Label htmlFor='format'>
+              Hình thức<span className='text-[#F12323]'>*</span>
+            </Label>
+            <Select id='format' required={true}>
+              <option>Offline</option>
+              <option>Online</option>
+            </Select>
+          </div>
+          <div id='checkbox'>
+            <div className='flex items-center gap-2'>
+              <Label htmlFor='accept'>Send to all member</Label>
+              <Checkbox id='accept' />
             </div>
           </div>
         </div>
@@ -90,7 +144,13 @@ export function ProposalParams() {
           </div>
         </div>
         <div className='px-5'>
-          <EmailProposal subject={params.subject} receiver={params.receiver} />
+          <EmailProposal
+            subject={params.subject}
+            receiver={params.receiver}
+            timeSchedule={params.timeSchedule}
+            dateSchedule={params.dateSchedule}
+            link={params.link}
+          />
         </div>
       </div>
     </div>
--- src/components/pages/projects/templates/EmailNoti.tsx
+++ src/components/pages/projects/templates/EmailNoti.tsx
@@ -1,6 +1,9 @@
 export interface EmailProposalProps {
   subject: string;
   receiver: string;
+  timeSchedule: string;
+  dateSchedule: string;
+  link: string;
 }
 
 export function EmailProposal(props: EmailProposalProps) {
@@ -8,42 +11,45 @@ export function EmailProposal(props: EmailProposalProps) {
     <div className='text-left text-[15px] leading-6 overflow-y-scroll h-96'>
       <div className='font-archivo mb-2'>
         <div className='space-x-2'>
-          <p className='inline '>Subject: {props.subject}</p>
+          <p className='inline'>Subject:</p>
+          <span className='underline text-[rgba(68,68,68,0.4)]'>{props.subject}</span>
         </div>
         <div className='space-x-2'>
-          <p className='inline'>Receiver: {props.receiver}</p>
+          <p className='inline'>Receiver:</p>
+          <span className='underline text-[rgba(68,68,68,0.4)]'>{props.receiver}</span>
         </div>
         <div className='space-x-2'>
           <p className='inline'>CC:</p>
-          <p className='italic underline inline text-[rgba(68,68,68,0.4)]'>CC</p>
+          <span className='underline text-[rgba(68,68,68,0.4)]'>CC</span>
         </div>
         <div className='space-x-2'>
           <p className='inline'>BCC:</p>
-          <p className='italic underline inline text-[rgba(68,68,68,0.4)]'>BCC</p>
+          <span className='underline text-[rgba(68,68,68,0.4)]'>BCC</span>
         </div>
       </div>
       <div>
-        <p className='italic inline'>Kính gửi</p>
-        <p className='italic underline inline text-[rgba(0,0,0,0.4)]'>Người nhận</p>
-        <p className='italic inline'>
-          ,
-          <br />
-        </p>
-        <p className='inline'>Dự án</p>
-        <p className='italic underline inline text-[rgba(0,0,0,0.4)]'>Tên dự án</p>
-        <p className='inline'>của</p>
-        <p className='italic underline inline text-[rgba(0,0,0,0.4)]'>Tên các thành viên nhóm</p>
-        <p className='inline'>đã hoàn thành proposal cho buổi thuyết minh đề tài. Em xin trân trọng gửi lời mời tới</p>
-        <p className='italic underline inline text-[rgba(0,0,0,0.4)]'>Người nhận</p>
+        <p className='italic inline'>Kính gửi </p>
+        <span className='italic underline text-[rgba(0,0,0,0.4)]'>
+          {props.receiver}
+          <span>
+            ,<br />
+          </span>
+        </span>
+        <p className='inline'>Dự án </p>
+        <p className='underline inline text-[rgba(0,0,0,0.4)]'>Tên dự án </p>
+        <p className='inline'>của </p>
+        <p className='underline inline text-[rgba(0,0,0,0.4)]'>Tên các thành viên nhóm </p>
+        <p className='inline'>đã hoàn thành proposal cho buổi thuyết minh đề tài. Em xin trân trọng gửi lời mời tới </p>
+        <span className='underline text-[rgba(0,0,0,0.4)]'>{props.receiver} </span>
         <p className='inline'>
           cùng tham gia làm thành viên hội đồng duyệt dự án lần này của bạn. Sau đây là thông tin về buổi duyệt:
           <br />
         </p>
-        <p className='inline'>▪️ Thời gian:</p>
-        <p className='italic underline inline text-[rgba(0,0,0,0.4)]'>
-          Lịch hẹn
+        <p className='inline'>▪️ Thời gian: </p>
+        <span className='italic underline text-[rgba(0,0,0,0.4)]'>
+          {props.timeSchedule} ngày {props.dateSchedule}
           <br />
-        </p>
+        </span>
         <p className='inline'>▪️ Hình thức:</p>
         <p className='italic underline inline text-[rgba(0,0,0,0.4)]'>
           Offline tại Lab (ONLINE: Đường link)
@@ -51,7 +57,7 @@ export function EmailProposal(props: EmailProposalProps) {
         </p>
         <p className='inline'>▪️ LINK file proposal:</p>
         <p className='italic underline inline text-[rgba(0,0,0,0.4)]'>
-          LINK proposal
+          <a href={props.link}>LINK proposal</a>
           <br />
         </p>
         <p className='inline'>
