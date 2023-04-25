58ae9289ce6f053eb76d3df8c03bbd89aaf46829
ngyngcphu <nguyenngocphu225@gmail.com>
2023-04-25 19:45:09 +0700

    autocomplete member name in input field

--- src/components/AutoSuggestForm.tsx
+++ src/components/AutoSuggestForm.tsx
@@ -1,71 +1,68 @@
 import React, { useState } from "react";
 import Autosuggest from "react-autosuggest";
-
-interface LanguagesProps {
-  name: string;
-  year: number;
-}
-
-// Imagine you have a list of languages that you'd like to autosuggest.
-const languages: LanguagesProps[] = [
-  {
-    name: "C",
-    year: 1972
-  },
-  {
-    name: "Elm",
-    year: 2012
-  }
-];
+import AutosuggestHighlightMatch from "autosuggest-highlight/match";
+import AutosuggestHighlightParse from "autosuggest-highlight/parse";
+import { MembersName } from "../name/MembersName";
 
 export function AutoSuggestForm() {
-  // Teach Autosuggest how to calculate suggestions for any given input value.
-  const getSuggestions = (value: string) => {
-    const inputValue = value.trim().toLowerCase();
-    const inputLength = inputValue.length;
-
-    return inputLength === 0 ? [] : languages.filter((lang) => lang.name.toLowerCase().slice(0, inputLength) === inputValue);
+  const escapeRegexCharacters = (str: string) => {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
   };
 
-  // When suggestion is clicked, Autosuggest needs to populate the input
-  // based on the clicked suggestion. Teach Autosuggest how to calculate the
-  // input value for every given suggestion.
-  const getSuggestionValue = (suggestion: LanguagesProps) => suggestion.name;
+  const getSuggestions = (value: string) => {
+    const escapedValue = escapeRegexCharacters(value.trim());
+    if (escapedValue === "") {
+      return [];
+    }
+    const regex = new RegExp("\\b" + escapedValue, "i");
+    return MembersName.filter((member) => regex.test(getSuggestionValue(member)));
+  };
 
-  // Use your imagination to render suggestions.
-  const renderSuggestion = (suggestion: LanguagesProps) => <div>{suggestion.name}</div>;
+  const getSuggestionValue = (suggestion: string) => suggestion;
 
-  // Autosuggest is a controlled component.
-  // This means that you need to provide an input value
-  // and an onChange handler that updates this value (see below).
-  // Suggestions also need to be provided to the Autosuggest,
-  // and they are initially empty because the Autosuggest is closed.
+  const renderSuggestion = (suggestion: string, { query }: { query: string }) => {
+    const suggestionText = suggestion;
+    const matches = AutosuggestHighlightMatch(suggestionText, query);
+    const parts = AutosuggestHighlightParse(suggestionText, matches);
+
+    return (
+      <span>
+        <span className=''>
+          {parts.map((part, index) => {
+            const className = "";
+
+            return (
+              <span className={className} key={index}>
+                {part.text}
+              </span>
+            );
+          })}
+        </span>
+      </span>
+    );
+  };
+
   const [value, setValue] = useState<string>("");
-  const [suggestions, setSuggestions] = useState<LanguagesProps[]>([]);
+  const [suggestions, setSuggestions] = useState<Array<string>>([]);
 
   const onChange = (event: React.FormEvent, { newValue }: Autosuggest.ChangeEvent) => {
     setValue(newValue);
   };
 
-  // Autosuggest will call this function every time you need to update suggestions.
-  // You already implemented this logic above, so just use it.
   const onSuggestionsFetchRequested = ({ value }: Autosuggest.SuggestionsFetchRequestedParams) => {
     setSuggestions(getSuggestions(value));
   };
 
-  // Autosuggest will call this function every time you need to clear suggestions.
   const onSuggestionsClearRequested = () => {
     setSuggestions([]);
   };
 
-  // Autosuggest will pass through all these props to the input.
   const inputProps = {
-    placeholder: "Type a programming language",
+    placeholder: "Type member name",
     value,
     onChange: onChange
   };
 
-  // Finally, render it!
   return (
     <Autosuggest
       suggestions={suggestions}
