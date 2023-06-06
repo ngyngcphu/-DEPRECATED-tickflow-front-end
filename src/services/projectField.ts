749ad8893c8a52a9a0cc23da684c0528b9848a0c
ngyngcphu <nguyenngocphu225@gmail.com>
2023-06-06 17:36:14 +0700

    feat: refactor detail project page

--- src/services/projectField.ts
+++ src/services/projectField.ts
@@ -1,6 +1,5 @@
 import { authHeader, server, invoke } from '@utils';
 
 export const projectFieldService = {
-  getField: () =>
-    invoke<string[]>(server.get('/auth/login/projectField', { headers: authHeader() }))
+  getField: () => invoke<string[]>(server.get('/projectField', { headers: authHeader() }))
 };
--- src/services/projectSummary.ts
+++ src/services/projectSummary.ts
@@ -1,8 +1,7 @@
 import { authHeader, server, invoke } from '@utils';
 
 export const projectSummaryService = {
-  getAll: () =>
-    invoke<ProjectSummary[]>(server.get('/auth/login/projects', { headers: authHeader() })),
+  getAll: () => invoke<ProjectSummary[]>(server.get('/projects', { headers: authHeader() })),
   getbyTag: (tag: string) =>
-    invoke<ProjectSummary[]>(server.get(`/auth/login/projects?status=${tag}`))
+    invoke<ProjectSummary[]>(server.get(`/projects?status=${tag}`, { headers: authHeader() }))
 };
